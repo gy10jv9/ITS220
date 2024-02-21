760de47cb59c06d62a602c97122cf997bffd5daf
Vincent Manalo <146041946+gy10jv9@users.noreply.github.com>
2024-02-21 17:09:47 +0800

    Delete volunteer-ms directory

--- volunteer-ms/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        <button onclick="location.href = 'members.php';"> Add Members </button>
-        <button onclick="location.href = 'inventory.php';"> Add Inventory </button>
-        <button onclick="location.href = 'works.php';"> Add Work Report </button>
-        <button onclick="location.href = 'donations.php';"> Add Donations </button>
-    </body>
-</html>
