784ca6965dc453e011a1606e091e202ed7dedfdb
Vincent Manalo <146041946+gy10jv9@users.noreply.github.com>
2024-02-21 17:08:56 +0800

    Add files via upload

---
+++ volunteer-ms/inventory.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <form style="width: 18vw; display: flex; flex-direction: column">
+            <p> First Name: </p> 
+            <input type = "text">
+            <p> Quantity: </p> 
+            <input type = "number">
+            <select>
+                <option> --Select Unit of Measurement-- </option>    
+                <option> pc </option>    
+                <option> kg </option>    
+                <option> L </option>       
+            <select>
+            <br/>
+            <input type = "submit">
+        </form>
+    </body>
+</html>
---
+++ volunteer-ms/works.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <form style="width: 18vw; display: flex; flex-direction: column">
+            <p> Beneficiary Name: </p> 
+            <input type = "text">
+            <p> Location: </p> 
+            <input type = "text">
+            <p> Expenses: </p> 
+            <input type = "number">
+            <br/>
+            <input type = "submit">
+        </form>
+    </body>
+</html>
