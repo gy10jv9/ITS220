035cee6ed3d7a471d9040533c9c709aec12179a4
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-02-23 06:32:34 +0800

    improved ui

--- members.php
+++ members.php
@@ -3,28 +3,88 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/fonts.css">
     </head>
     <body>
         <div class="form-container">
-            <header class="header-grid">
-                <section>
-                    <div class="logo"> This is only a placeholder logo </div>
-                </section>
-                <section></section>
-            </header>
-            <button onclick="location.href = 'index.php';"> Back </button>
-            <form style="width: 18vw; display: flex; flex-direction: column">
-            <p> First Name: </p> 
-            <input type = "text">
-            <p> Second Name: </p>
-            <input type = "text">
-            <select>
-                <option> --Select Sex-- </option>    
-                <option> Male </option>    
-                <option> Female </option>    
-            <select>
-            <br/>
-            <input type = "submit">            
+            <div class="label-container">
+                <img src="img/icon-back.png" class="back-bttn" onclick="location.href = 'index.php'">
+                <p class="label"> Register a Member </p>
+            </div>
+            <main>
+                <form>
+                    <h1> Enter Primary Information </h1>
+                    <div class="input-container">
+                        <div class="fgrow-1">
+                            <p> First Name </p> 
+                            <input type = "text">
+                        </div>
+                        <div class="fgrow-1">
+                            <p> Last Name </p>
+                            <input type = "text">
+                        </div>
+                    </div>
+                    <div class="input-container">
+                        <div class="fgrow-2">
+                            <p> Contact Number </p> 
+                            <input type = "text">
+                        </div>
+                        <div class="fgrow-2">
+                            <p> Nationality </p> 
+                            <input type = "text">
+                        </div>
+                        <div class="select-container fgrow-1">
+                            <select>
+                                <option> -- Select Sex -- </option>    
+                                <option> Male </option>    
+                                <option> Female </option>    
+                            <select>
+                        </div>
+                        <div class="select-container fgrow-1">
+                            <select>
+                                <option> -- Select Civil Status -- </option>    
+                                <option> Male </option>    
+                                <option> Female </option>    
+                            <select>
+                        </div>
+                    </div>
+
+                    <hr/>
+                    <h1> Enter Primary Address </h1>
+                    <div class="input-container m-0">
+                        <div class="fgrow-1">
+                            <p> Street Address </p> 
+                            <input type = "text">
+                        </div>
+                    </div>
+                    <div class="input-container">
+                        <div class="fgrow-1">
+                            <p> City/Province </p> 
+                            <input type = "text">
+                        </div>
+                        <div class="fgrow-1">
+                            <p> State/Region </p> 
+                            <input type = "text">
+                        </div>
+                        <div class="fgrow-1">
+                            <p> Country </p> 
+                            <input type = "text">
+                        </div>
+                    </div>
+
+                    <br/>
+                    <div class="input-container">
+                        <input type="submit" class="bttn-primary fgrow-1" value="Register Member">
+                        <button class="bttn-cancel" onclick="event.preventDefault(); location.href = 'index.php'"> Cancel </button>
+                    </div>
+                </form>  
+            </main>       
         </div>
     </body>
+    <script>
+        // Add this script to handle the cancel button click event
+        document.querySelector('.bttn-cancel').addEventListener('click', function() {
+            location.href = 'index.php';
+        });
+    </script>
 </html>
