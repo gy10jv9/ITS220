84b4f2b15024c54a5dfb6240ef8fb22b98b84816
ppgs <vincentm.work.2023@gmail.com>
2024-02-22 17:01:18 +0800

    added style

--- members.php
+++ members.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+
 <html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
     <body>
-        <button onclick="location.href = 'index.php';"> Back </button>
-        <form style="width: 18vw; display: flex; flex-direction: column">
+        <div class="form-container">
+            <header class="header-grid">
+                <section>
+                    <div class="logo"> This is only a placeholder logo </div>
+                </section>
+                <section></section>
+            </header>
+            <button onclick="location.href = 'index.php';"> Back </button>
+            <form style="width: 18vw; display: flex; flex-direction: column">
             <p> First Name: </p> 
             <input type = "text">
             <p> Second Name: </p>
@@ -12,7 +24,7 @@
                 <option> Female </option>    
             <select>
             <br/>
-            <input type = "submit">
-        </form>
+            <input type = "submit">            
+        </div>
     </body>
 </html>
