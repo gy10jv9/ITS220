223ef092dda5fd08a9b6ac62a639c714c0a9b64a
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-03-21 22:15:49 +0800

    added availability

--- add-volunteer.php
+++ add-volunteer.php
@@ -102,6 +102,50 @@
                         </div>
                     </div>
 
+                    <hr/>
+                    <h1> Indicate Availabilty </h1>
+                    <section class="f-row">
+                        <div class="fgrow-1">
+                            <p> Days of the Week </p> 
+                            <input type="checkbox">
+                            <label> Sunday </label><br/>
+                            <input type="checkbox">
+                            <label> Monday </label><br/>
+                            <input type="checkbox">
+                            <label> Tuesday </label><br/>
+                            <input type="checkbox">
+                            <label> Wednesday </label><br/>
+                            <input type="checkbox">
+                            <label> Thursday </label><br/>
+                            <input type="checkbox">
+                            <label> Friday </label><br/>
+                            <input type="checkbox">
+                            <label> Saturday </label><br/>
+                        </div>
+                        <div class="fgrow-1">
+                            <p> Time of the Day </p> 
+                            <input type="checkbox">
+                            <label> Morning </label><br/>
+                            <input type="checkbox">
+                            <label> Afternoon </label><br/>
+                            <input type="checkbox">
+                            <label> Evening </label><br/>
+                        </div>
+                        <div class="fgrow-1">
+                            <p> Frequency </p> 
+                            <input type="radio" name="frequency">
+                            <label> Daily </label><br/>
+                            <input type="radio" name="frequency">
+                            <label> Weekly </label><br/>
+                            <input type="radio" name="frequency">
+                            <label> Biweekly </label><br/>
+                            <input type="radio" name="frequency">
+                            <label> Monthly </label><br/>
+                            <input type="radio" name="frequency">
+                            <label> Occasional </label><br/>
+                        </div>
+                    </section>
+
                     <br/>
                     <div class="input-container">
                         <input type="submit" class="bttn-primary fgrow-1" value="Add Volunteer">
@@ -109,6 +153,7 @@
                     </div>
                 </form>
             </main>
+
             <footer>
                 <p> The Red Amity Volunteer Services, a humanitarian organization recognized worldwide, operates on the principles of volunteerism and the desire to alleviate human suffering. It is an institution powered by the spirit of selflessness, where individuals contribute their time, energy, and resources to aid those in need. In the Philippines, the Red Amity has a robust volunteer program, Red Amity Cross, which is a community-based network of volunteers who are trained and prepared to respond to disasters and promote health and welfare services. Volunteering with the Red Amity does not only involve emergency response but also encompasses a wide range of activities including blood donation drives, health services, and youth programs. The organization emphasizes that while it does not provide financial compensation, it offers a sense of fulfillment and purpose, as volunteers are integral to the delivery of essential services. Those interested in volunteering can apply online and are required to undergo orientation and training to equip them with the necessary skills for effective service. The Red Amity is a testament to the power of collective human effort and compassion in building resilient communities </p>
             </footer>       
