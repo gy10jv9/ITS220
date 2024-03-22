5f39b47335c249445006b56928944d48c5ffd34b
ppgs <vincentm.work.2023@gmail.com>
2024-03-22 10:46:18 +0800

    updated dashboard

--- dashboard.php
+++ dashboard.php
@@ -19,14 +19,60 @@
             </div>
         </div>
 
-        <div class="container-main w-full">
-            <section>
-                <h2> Welcome, </h2>
-                <h1> Admin </h1>
-            </section>
-            <div class="chart-container">
-    </div>
-  <script src="js/script.js"></script>
+        <div class="container-main container-dashboard">
+            <main>
+                <header>
+                    <div class="logo"></div>
+                    <div class="container-details">
+                        <h1> philippine red amity Volunteer Services </h1>
+                        <p> 123 Rizal Avenue, Barangay Poblacion 5, Bacolod City, Negros Occidental, 6100 </p>
+                        <hr/>
+
+                        <div class="container-socials">
+                            <div class="social-link">
+                                <div class="icon fb" ></div>
+                                <p> Red Amity Volunteer PH </p>
+                            </div>
+                            <div class="social-link">
+                                <div class="icon ig" ></div>
+                                <p> @redamityph </p>
+                            </div>
+                            <div class="social-link">
+                                <div class="icon yt" ></div>
+                                <p> Red Amity PH </p>
+                            </div>
+                            <div class="social-link">
+                                <div class="icon email" ></div>
+                                <p> redamityph@gmail.com </p>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <img src="img/chart3.png" class="lineGraph"></img>
+            </main>
+
+            <div class="otherDetails">
+                <div class="list summary">
+                    <h2 class="text-center" style="margin-top: 8%;"> No Volunteers Yet </h2>
+                    <p class="text-center"> click "View All Volunteers" to view dashboard </p>
+                    <button onclick="location.href='dash-volunteers.php'" style="margin-bottom: 10%;"> View All Volunteers </button>
+                </div>
+                <div class="list summary">
+                    <h2 class="text-center" style="margin-top: 8%;"> No Donations Yet </h2>
+                    <p class="text-center"> click "View All Donations" to view dashboard </p>
+                    <button onclick="location.href='dash-donations.php'" style="margin-bottom: 10%;"> View All Donations </button>
+                </div>
+                <div class="list summary">
+                    <h2 class="text-center" style="margin-top: 8%;"> No Inventory Yet </h2>
+                    <p class="text-center"> click "View All Inventory" to view dashboard </p>
+                    <button onclick="location.href='dash-inventory.php'" style="margin-bottom: 10%;"> View All Inventory </button>
+                </div>
+                <div class="list summary">
+                    <h2 class="text-center" style="margin-top: 8%;"> No Services Yet </h2>
+                    <p class="text-center"> click "View All Services" to view dashboard </p>
+                    <button onclick="location.href='dash-serviceReport.php'" style="margin-bottom: 10%;"> View All Services </button>
+                </div>
+            </div>
         </div>
     </body>
 </html>
