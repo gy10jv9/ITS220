c27ea1849d817a05cf0cf6fbf5552d271f8a1d88
mc509 <vincentm.work.2023@gmail.com>
2024-04-05 17:45:23 +0800

    added profile class

---
+++ classes/Profile.php
@@ -0,0 +1,13 @@
+<?php
+    class Profile {
+        function addprofile() {
+
+        }
+        function viewProfile() {
+
+        }
+        function searchProfile() {
+            
+        }
+    }
+?>
