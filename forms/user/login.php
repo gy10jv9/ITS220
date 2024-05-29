5619f3b222d59955e960c16ec362f420ad6e1b85
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-05-29 15:59:56 +0800

    updated ui

--- forms/user/login.php
+++ forms/user/login.php
@@ -7,15 +7,16 @@
                 <div class="icon email"></div>
                 <input type="text" name="username" placeholder="enter username"/>
             </div>
-            <div class="f-row relative">
+            <div class="f-row relative mt-pt25">
                 <div class="icon lock"></div>
                 <input type="password" name="password" placeholder="enter password"/>
             </div>
 
-            <button name="login" class="bttn-primary2"> Sign in </button>
-            <p> Forgot password? <a href="login.php"> Click here </a></p>
-            <p> Don't have an account?</p>
-            <button type="button" onclick="location.href='register.php'"> Register </button>
+            <button name="login" class="bttn-primary2 mt-1"> Sign in </button>
+            <p class="size-14 grey-2"> Forgot password? <a href="login.php" class="size-14 grey-2"> Click here </a></p>
+
+            <p class="size-14 grey-2 mt-1"> Don't have an account?</p>
+            <button type="button" class="bttn-secondary" onclick="location.href='register.php'"> Register </button>
         </form>
 
         <div class="poster" style="background: url(../../img/service-1.jpg) center center/cover;"></div>
