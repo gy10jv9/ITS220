93f26126608979b7fcf8bc4c357e0222f8f8e380
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-05-29 15:13:55 +0800

    updated ui

--- forms/user/login.php
+++ forms/user/login.php
@@ -1,21 +1,25 @@
 <?php include("../../partials/form-header.php"); ?>
 
-<div class="fullWindow2 centerContent2">
-    <form action="_login.php" method="post">
-        <div class="f-row relative">
-            <div class="icon email"></div>
-            <input type="text" name="username" placeholder="enter username"/>
-        </div>
-        <div class="f-row relative">
-            <div class="icon lock"></div>
-            <input type="password" name="password" placeholder="enter password"/>
-        </div>
+<div class="fullWindow2 centerContent2" style="background-color: #e2e6ec;">
+    <div class="container-login">
+        <form action="_login.php" method="post">
+            <div class="f-row relative">
+                <div class="icon email"></div>
+                <input type="text" name="username" placeholder="enter username"/>
+            </div>
+            <div class="f-row relative">
+                <div class="icon lock"></div>
+                <input type="password" name="password" placeholder="enter password"/>
+            </div>
 
-        <button name="login"> Sign in </button>
-        <p> Forgot password? <a href="login.php"> Click here </a></p>
-        <p> Don't have an account?</p>
-        <button type="button" class="login" onclick="location.href='register.php'"> Register </button>
-    </form>
+            <button name="login" class="bttn-primary2"> Sign in </button>
+            <p> Forgot password? <a href="login.php"> Click here </a></p>
+            <p> Don't have an account?</p>
+            <button type="button" onclick="location.href='register.php'"> Register </button>
+        </form>
+
+        <div class="poster" style="background: url(../../img/service-1.jpg) center center/cover;"></div>
+    </div>
 </div>
 
 <?php include("../../partials/form-footer.php") ?>
