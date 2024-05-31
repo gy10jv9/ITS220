350d15f422f38a315031084646012a03e840c0e3
vincentm.work.2023@gmail.com <vincentm.work.2023@gmail.com>
2024-05-31 15:10:56 +0800

    fixed ui in login page

--- forms/user/login.php
+++ forms/user/login.php
@@ -5,20 +5,22 @@
         <form action="_login.php" method="post">
             <div class="logo-1"></div>
 
-            <div class="f-row relative">
-                <div class="icon email"></div>
-                <input type="text" name="username" placeholder="enter username"/>
-            </div>
-            <div class="f-row relative mt-pt25">
-                <div class="icon lock"></div>
-                <input type="password" name="password" placeholder="enter password"/>
-            </div>
+            <div class="container-inputs">
+                <div class="f-row relative">
+                    <div class="icon email"></div>
+                    <input type="text" name="username" placeholder="enter username"/>
+                </div>
+                <div class="f-row relative mt-pt25">
+                    <div class="icon lock"></div>
+                    <input type="password" name="password" placeholder="enter password"/>
+                </div>
 
-            <button name="login" class="bttn-primary2 mt-1"> Sign in </button>
-            <p class="size-14 grey-2"> Forgot password? <a href="login.php" class="size-14 grey-2"> Click here </a></p>
+                <button name="login" class="bttn-primary2 mt-1"> Sign in </button>
+                <p class="size-14 grey-2"> Forgot password? <a href="login.php" class="size-14 grey-2"> Click here </a></p>
 
-            <p class="size-14 grey-2 mt-1"> Don't have an account?</p>
-            <button type="button" class="bttn-secondary" onclick="location.href='register.php'"> Register </button>
+                <p class="size-14 grey-2 mt-1"> Don't have an account?</p>
+                <button type="button" class="bttn-secondary" onclick="location.href='register.php'"> Register </button>
+            </div>
         </form>
 
         <div class="poster" style="background: url(../../img/service-1.jpg) center center/cover;"></div>
