61cbbff91dcc13569ab5df952d0743fac4e06643
ppgs <vincentm.work.2023@gmail.com>
2024-06-04 10:15:13 +0800

    added approval of roles

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ include("./classes/Request.php");
 $db = new Database();
 $Request = new Request($db);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['approve'])) {
+        if ($_POST['request_role'] == 2) { // if ang request is admin role
+            $Request->approveAdminRole($_POST['request_id']);
+        } elseif ($_POST['request_role'] == 1) { // if ang request is super admin role
+            $Request->approveSuperAdminRole($_POST['request_id']);
+        }
+    } elseif (isset($_POST['decline'])) {
+        $Request->decline($_POST['request_id']);
+    }
+}
+
 $roleRequests = $Request->getallRoleRequests();
 ?>
 
@@ -64,31 +76,28 @@ $roleRequests = $Request->getallRoleRequests();
                 <div class="superAdmin-dashboard">
                     <div class="request-superAdminRole">
                         <h1> Super Admin Role Requests </h1>
-               <!-- /////[ TABLE ]///// -->
-               <table cellpadding="5" style="margin-top: 1rem;">
-                    <tr style="border: 2px solid #81171b;">
-                        <td style="border: 2px solid #81171b;"><h2> Fullname </h2></td>
-                        <td style="border: 2px solid #81171b;"><h2> Contact Num </h2></td>
-                        <td class="text-center" style="border: 2px solid #81171b;"><h2> Actions </h2></td>
-                    </tr>
-                    <?php foreach($volunteers as $vol) { ?>
-                        <tr>
-                            <td><?php echo $vol["firstName"]. " ". $vol["lastName"] ?></td>
-                            <td><?php echo $vol["contactNumber"] ?></td>
-                            <td class="text-center">
-                                <a href="http://localhost/its220-infoman/forms/volunteer/view-volunteer.php?id=<?= isset($vol['volunteer_id'])? (int) $vol['volunteer_id'] : 0?>">
-                                     <img src ='img/view.png' width = '16' >
-                                </a>
-                                <a href="http://localhost/its220-infoman/forms/volunteer/updateForm-volunteer.php?id=<?= isset($vol['volunteer_id'])? (int) $vol['volunteer_id'] : 0?>">
-                                    <img src ='img/edit.png' width = '16' >
-                                </a>
-                                <a href="http://localhost/its220-infoman/forms/volunteer/delete-volunteer.inc.php?id=<?= isset($vol['volunteer_id'])? (int) $vol['volunteer_id'] : 0?>">
-                                    <img src ='img/delete.png' width = '16' alt="Delete">
-                                </a>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </table>
+                        <!-- -----[ ROLE REQUESTS TABLE ]----- -->
+                        <table cellpadding="5" style="margin-top: 1rem;" class="w-full">
+                            <tr style="border: 2px solid #81171b;">
+                                <td style="border: 2px solid #81171b;"><h2> Username </h2></td>
+                                <td style="border: 2px solid #81171b;"><h2> Role </h2></td>
+                                <td class="text-center" style="border: 2px solid #81171b;"><h2> Actions </h2></td>
+                            </tr>
+                            <?php foreach ($roleRequests as $request): ?>
+                                <tr>
+                                    <td><?php echo $request['username']; ?></td>
+                                    <td><?php echo $request['type']; ?></td>
+                                    <td class="text-center">
+                                        <form method="post">
+                                            <input type="hidden" name="request_id" value="<?php echo $request['id']; ?>">
+                                            <input type="hidden" name="request_role" value="<?php echo $request['type_id']; ?>">
+                                            <button name="approve" type="submit">Approve</button>
+                                            <button name="decline" type="submit">Decline</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </table>
                     </div>
                     <div class="request-others">
                         <h1> Recent Requests </h1>
